49e1734a3a85d3e9e73d694061af2b927c634665
xgabrielle <gabrielle.bergman@student.forsbergsskola.se>
2023-09-05 13:37:14 +0100

    P6

---
+++ P6DataTypes/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string outputString= "Hello World";
+float decimalValue = 0.3f;
+bool isAwesome = true;
+byte hexByte = 0xa7;
+
+Console.WriteLine(outputString+"\n"+decimalValue+"\n"+isAwesome+"\n"+hexByte);
