ef18657648d4948fccc0f5643a4d98ab68046d16
xgabrielle <gabrielle.bergman@student.forsbergsskola.se>
2023-09-05 08:16:59 +0100

    print HelloWorld

--- P3PrintingOutput/Program.cs
+++ P3PrintingOutput/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello World! \nWelcome to 102 CSharp Basics! \nIt's easy to print outputs... " +
+                  "\n...to the console!");
