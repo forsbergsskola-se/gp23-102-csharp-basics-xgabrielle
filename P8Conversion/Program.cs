b82f51f93e6bc2d0fba0bcde63ef401e09f0bbe2
xgabrielle <gabrielle.bergman@student.forsbergsskola.se>
2023-09-06 07:23:27 +0100

    P8 on our way, not quite there

---
+++ P8Conversion/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Give me a number.");
+
+string number = Console.ReadLine();
+
+double dbl = Convert.ToDouble(number);
+Console.WriteLine(dbl);
+
+int sgl = Convert.ToInt32(dbl);
+Console.WriteLine(sgl);
+
+int og = Convert.ToInt32(number);
+Console.WriteLine(og);
+
+// Idea??
+/* 
+Console.ReadLine() = whole;
+double whole = number;
+int i = Convert.ToInt32(whole);
+*/
