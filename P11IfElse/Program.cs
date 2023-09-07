770334a8c8bf4d491fa1c4bca6d4caa2f5560bb0
xgabrielle <gabrielle.bergman@student.forsbergsskola.se>
2023-09-07 14:23:33 +0100

    P11 done

--- P11IfElse/Program.cs
+++ P11IfElse/Program.cs
@@ -1,27 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-bool child;
-bool teenager;
-bool adult;
-
 Console.WriteLine("What's you're age?");
 int ageInput = int.Parse(Console.ReadLine());
 
 if (ageInput <= 12)
 {
-    
     Console.WriteLine("You're a child");
 }
 
 else if (ageInput >= 13 && ageInput <= 18)
 {
-    
     Console.WriteLine("You're a teenager");
 }
 
 else
 {
-    
     Console.WriteLine("You're an adult");
 }
 
@@ -32,9 +25,27 @@ int secondNumberInput = int.Parse(Console.ReadLine());
 if (secondNumberInput > ageInput)
 {
     Console.WriteLine($"The maximum is: {secondNumberInput}");
+    if (secondNumberInput % 2 == 0)
+    {
+        Console.WriteLine("Your number is even");
+    }
+    else
+    {
+        Console.WriteLine("Your number is odd");
+    }
 }
 
 else
 {
     Console.WriteLine($"The maximum number is: {ageInput}");
+    if (ageInput % 2 == 0)
+    {
+        Console.WriteLine("Your number is even");
+    }
+    else
+    {
+        Console.WriteLine("Your number is odd");
+    }
 }
+
+
