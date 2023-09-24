223dee3e3f9019a1d9e2fa93380b9d3209a46883
xgabrielle <gabrielle.bergman@student.forsbergsskola.se>
2023-09-24 18:13:54 +0100

    updated number game

--- P13GoTo/Program.cs
+++ P13GoTo/Program.cs
@@ -7,26 +7,19 @@ Console.WriteLine("Choose a number between 1-100");
     int userGuess = int.Parse(Console.ReadLine());
 
 
-    if (userGuess < winningNumber)
-    {
-        Console.WriteLine("Higher");
-        goto TryAgain;
-    }
+if (userGuess < winningNumber)
+{
+    Console.WriteLine("Higher");
+    goto TryAgain;
+}
 
-    if (userGuess > winningNumber)
-    {
-        Console.WriteLine("Lower");
-        goto TryAgain;
-    }
+if (userGuess > winningNumber)
+{
+    Console.WriteLine("Lower");
+    goto TryAgain;
+}
 
-    if (userGuess == winningNumber)
+if (userGuess == winningNumber)
 {
     Console.WriteLine("Well done, you guessed right!");
 }
-
-
-    
-
-
-
-
