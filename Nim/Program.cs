64b75e2dedb630a69dc10be6a3cfa72f4fea9cfb
xgabrielle <gabrielle.bergman@student.forsbergsskola.se>
2023-09-26 11:14:35 +0200

    ai  last draw sorted

--- Nim/Program.cs
+++ Nim/Program.cs
@@ -1,10 +1,12 @@
 ﻿Console.WriteLine("Welcome to Nim");
 int matches = 24;
+int aiDraw = Random.Shared.Next(1,4);
 
 drawAgain:
+Console.WriteLine("||||||||||||||||||||||||");
 Console.WriteLine("Remove 1-3 matches");
 Console.WriteLine("User");
-//int draw = int.Parse(Console.ReadLine());
+string matchVisual = "|";
 if (!int.TryParse(Console.ReadLine(), out int draw))
 {
     Console.WriteLine("You can't do that, choose 1-3 please");
@@ -17,16 +19,26 @@ if (draw > 3 ||(draw <=0))
     goto drawAgain;
 }
 
+
+
+if (matches == 3)
+    aiDraw = 2;
+
+
+if (matches == 2)
+    aiDraw = 1;
+
 matches = matches - draw;
 if (matches > 1)
 {
     
     Console.WriteLine(matches);
-    int aiDraw = Random.Shared.Next(1,4);
+    
     Console.WriteLine("Ai");
     Console.WriteLine(aiDraw);
     Console.WriteLine(matches-aiDraw);
     matches -= aiDraw;
+    
     
     if (matches > 1)
     {
